b3f2854ce8829423b1c13b9ff9d79155421e2ca8
Rafael-pin <rafa.pinheiro.pinheiro@gmail.com>
2022-03-01 14:18:16 -0300

    deleting and creating proposals

--- basic_crud/app/controllers/api/v1/proposals_controller.rb
+++ basic_crud/app/controllers/api/v1/proposals_controller.rb
@@ -1,6 +1,8 @@
 module Api
     module V1
         class ProposalsController < ApplicationController   
+            skip_before_action :verify_authenticity_token
+
             def index
                 proposals = Proposal.order('created_at DESC');
                 render json: {status: 'SUCCESS', message:'Propostas carregadas', data:proposals},status: :ok
@@ -10,6 +12,27 @@ module Api
 				proposal = Proposal.find(params[:id])
 				render json: {status: 'SUCCESS', message:'Proposta carregada', data:proposal},status: :ok
 			end
+
+            def create
+				proposal = Proposal.new(proposal_params)
+				if proposal.save
+					render json: {status: 'SUCCESS', message:'Proposta salva', data:proposal},status: :ok
+				else
+					render json: {status: 'ERROR', message:'Proposta não salva', data:proposal.erros},status: :unprocessable_entity
+				end
+			end
+
+            def destroy
+				proposal = Proposal.find(params[:id])
+				proposal.destroy
+				render json: {status: 'SUCCESS', message:'Deleted proposal', data:proposal},status: :ok
+			end
+
+            private
+			def proposal_params
+				params.permit(:name, :description, :value, :start_date, :end_date)
+			end
+
         end
     end
 end

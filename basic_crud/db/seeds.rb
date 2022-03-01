46b148c7c60f191986d79373ea5beef3c18daa5d
Rafael-pin <rafa.pinheiro.pinheiro@gmail.com>
2022-03-01 11:24:42 -0300

    add faker to generate seeds

--- basic_crud/db/seeds.rb
+++ basic_crud/db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do
+
+    Proposal.create({
+        name: Faker::Company.name,
+        description: Faker::Music.album,
+        value: Faker::Number.decimal(l_digits: 2),
+        start_date: Faker::Date.backward(days: 100),
+        end_date: Faker::Date.forward(days: 100),
+    })
+
+end
